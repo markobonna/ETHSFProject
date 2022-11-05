6cdf686c58dad4a1662584cb88998d5a9d982133
dev <69097418+markobonna@users.noreply.github.com>
2022-11-05 11:30:56 -0700

    fans placeholder page

---
+++ pages/fans.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import FanPlaceholder from "@/components/FanPlaceholder";
+
+export default function Fans() {
+  return (
+    <>
+      <Head>
+        <title>Fan Clubs</title>
+      </Head>
+      <div>This is the Fan Clubs Page</div>
+    </>
+  );
+}
