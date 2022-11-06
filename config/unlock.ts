228bac8514c63537336177936ae4f354f91e7597
markobonna <69097418+markobonna@users.noreply.github.com>
2022-11-06 01:03:52 -0700

    Update unlock.ts

--- config/unlock.ts
+++ config/unlock.ts
@@ -1,10 +1,23 @@
 export const paywallConfig: Record<string, unknown> = {
   locks: {
-    "0x6e2ebf51da851bfdd53821ce369315391040c1ea": {
+    "0xa0cce668213314bd4bae295d7ac0b727042cec05": {
       network: 80001,
+      name: "Swifties Club",
+      recurringPayments: "",
+    },
+
+    "0x0881f37f826468e7379c2b50fafc88c83496c551": {
+      network: 80001,
+      name: "Dogghouse Club",
+      recurringPayments: "",
     },
   },
   pessimistic: true,
-  title: "Taylor Swift Fan Club",
-  icon: "https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Ff4%2Fd9%2F86%2Ff4d986ee27bfe77768b4c58ab0dfd996.jpg&sp=1667682027T2f5a200502c7737b617558141c86ae2b7285c72aef655e329adc98af62eba1cf",
+  title: "JUKEBOX",
+  icon: "https://nftstorage.link/ipfs/bafkreibmz6bbjdyzro44hhczpcc3oqus7gaeazumwymmvegx6qetbtbkdm",
+  redirectUri: "https://ethsf-project.vercel.app/fans",
+  persistentCheckout: false,
+  referrer: "0xcba09419248ED763522b9435342462F984A3944d",
+  messageToSign: "",
+  hideSoldOut: false,
 };
