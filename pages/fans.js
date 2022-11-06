9bd615861cdd60a905ce6d9b7041c3b56dc970e9
dev <69097418+markobonna@users.noreply.github.com>
2022-11-05 20:43:28 -0700

    Update fans.js

--- pages/fans.js
+++ pages/fans.js
@@ -10,8 +10,34 @@ export default function Fans() {
         <title>Fan Clubs</title>
       </Head>
       <div>This is the Fan Clubs Page</div>
-      <div>Taylor Swift Fan Club</div>
-      <div>Snoop Dogg Fan Club</div>
+      <div className="max-w-screen-md px-6 pt-24 mx-auto">
+        <header className="pb-4 space-y-4 text-center border-b-2">
+          <h1 className="text-5xl font-bold">
+            Available NFT Fan Club memberships
+          </h1>
+          <p className="text-xl text-gray-700">
+            You do not have valid NFT membership to access this page. Verify or
+            buy membership by using the login button below.
+          </p>
+        </header>
+        <div className="pt-8">
+          <div className="flex justify-center">
+            <button>
+              <Link href="/api/login">Purchase an NFT Club Membership</Link>{" "}
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button>
+          <Link href="/taylor">Swifties Club</Link>{" "}
+        </button>
+      </div>
+      <div>
+        <button>
+          <Link href="/snoop">Dogghouse Club</Link>{" "}
+        </button>
+      </div>
     </>
   );
 }
