e2b916ed8712abda0365e7e82261c1b7a1c992b6
mubarakone <52806204+mubarakone@users.noreply.github.com>
2022-11-05 21:06:19 -0700

    Switch to Polygon Testnet + Connect Wallet

--- components/ConnectWallet.js
+++ components/ConnectWallet.js
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react"
 export default function ConnectWallet() {
   const [provider, setProvider] = useState({})
 
+  //Check if MetaMask exists
   useEffect( async () => {
        if (typeof window.ethereum !== 'undefined') {
            const provider = new ethers.providers.Web3Provider(window.ethereum)
@@ -16,9 +17,27 @@ export default function ConnectWallet() {
   const [connected, setConnected] = useState(false)
   const [account, setAccount] = useState("Empty")
 
+  //Connect through MetaMask
   async function connect() {
     try {
-      await provider.send("eth_requestAccounts", []);
+// Switch to Polygon Network
+      await window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [{
+            chainId: "0x13881",
+            rpcUrls: ["https://rpc-mumbai.maticvigil.com/"],
+            chainName: "Mumbai Testnet",
+            nativeCurrency: {
+                name: "MATIC",
+                symbol: "MATIC",
+                decimals: 18,
+            },
+            blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
+        }],
+    });
+
+    // Connect Wallet
+    await provider.send("eth_requestAccounts", []);
       setConnected(true)
       const signer = await provider.getSigner()
       if (signer !== null || undefined) {
