beef8dda53f1f4b52febede93fdb1c9f6661d830
dev <69097418+markobonna@users.noreply.github.com>
2022-11-05 20:33:56 -0700

    config anf hooks for unlock

---
+++ config/unlock.ts
@@ -0,0 +1,10 @@
+export const paywallConfig: Record<string, unknown> = {
+  locks: {
+    "0x6e2ebf51da851bfdd53821ce369315391040c1ea": {
+      network: 80001,
+    },
+  },
+  pessimistic: true,
+  title: "Taylor Swift Fan Club",
+  icon: "https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Ff4%2Fd9%2F86%2Ff4d986ee27bfe77768b4c58ab0dfd996.jpg&sp=1667682027T2f5a200502c7737b617558141c86ae2b7285c72aef655e329adc98af62eba1cf",
+};
