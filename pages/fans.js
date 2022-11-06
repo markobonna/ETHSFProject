323a33548285831ddf58337b0b25ce434b56db9b
dev <69097418+markobonna@users.noreply.github.com>
2022-11-05 20:50:21 -0700

    unlock apis

--- pages/fans.js
+++ pages/fans.js
@@ -9,16 +9,22 @@ export default function Fans() {
       <Head>
         <title>Fan Clubs</title>
       </Head>
-      <div>This is the Fan Clubs Page</div>
+
       <div className="max-w-screen-md px-6 pt-24 mx-auto">
         <header className="pb-4 space-y-4 text-center border-b-2">
-          <h1 className="text-5xl font-bold">
-            Available NFT Fan Club memberships
-          </h1>
-          <p className="text-xl text-gray-700">
-            You do not have valid NFT membership to access this page. Verify or
-            buy membership by using the login button below.
-          </p>
+          <h1 className="text-5xl font-bold">Available NFT Club memberships</h1>
+          <div>
+            <div>
+              <button>
+                <Link href="/taylor">Swifties Club</Link>{" "}
+              </button>
+            </div>
+            <div>
+              <button>
+                <Link href="/snoop">Dogghouse Club</Link>{" "}
+              </button>
+            </div>
+          </div>
         </header>
         <div className="pt-8">
           <div className="flex justify-center">
--- pages/snoop.js
+++ pages/snoop.js
@@ -46,7 +46,7 @@ function Snoop() {
       <div className="bg-white">
         <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
           <h2 className="text-xl font-bold text-gray-900">
-            This Month's Snoop Dogg Fan Club Experiences
+            This Month's Dogghouse Club Experiences
           </h2>
 
           <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
